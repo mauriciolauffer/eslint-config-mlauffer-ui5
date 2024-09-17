bac03873e5a1204dd96e521d032187082afb2d82
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2024-09-17 15:09:48 +1000

    fix: Remove compat layer

--- test/config.test.js
+++ test/config.test.js
@@ -3,10 +3,10 @@ import config from "../lib/index.js";
 
 describe("Config object", () => {
   it("should export rules", () => {
-    const configDOM = config[config.length - 2];
+    const configDOM = config[config.length - 2][0];
     expect(Array.isArray(config)).toBeTruthy();
     expect(config.length).toBeGreaterThan(0);
-    expect(configDOM.name).toEqual("no-unsanitized/DOM");
+    expect(configDOM.name).toEqual(undefined);
     expect(configDOM.rules).toBeTypeOf("object");
   });
 });
