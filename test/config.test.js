b873941bb038d11d39d449cdf81050e22b9fe7e4
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2024-10-23 19:02:41 +1000

    feat!: SonarJS + PNPM (#32)
    * feat!: SonarJS + PNPM
    
    * chore: Bump deps

--- test/config.test.js
+++ test/config.test.js
@@ -3,10 +3,17 @@ import config from "../lib/index.js";
 
 describe("Config object", () => {
   it("should export rules", () => {
-    const configDOM = config[config.length - 2][0];
+    const configDOM = config[0];
     expect(Array.isArray(config)).toBeTruthy();
     expect(config.length).toBeGreaterThan(0);
     expect(configDOM.name).toEqual(undefined);
+    expect(configDOM.plugins).toBeTypeOf("object");
     expect(configDOM.rules).toBeTypeOf("object");
+    expect(config).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ name: "mlauffer-ui5" }),
+        expect.objectContaining({ name: "mlauffer-nodejs" }),
+      ]),
+    );
   });
 });
