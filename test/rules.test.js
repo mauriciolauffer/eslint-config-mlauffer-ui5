72b3c4e818b34c298586fbde52f101692d72eee3
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2024-10-23 21:54:03 +1300

    feat!: SonarJS + PNPM

--- test/rules.test.js
+++ test/rules.test.js
@@ -1,53 +1,55 @@
 import { describe, it, expect } from "vitest";
-import config from "../lib/index.js";
 import { RuleTester } from "eslint";
+import config from "../lib/index.js";
 
 const tester = new RuleTester();
 
-describe("Execute rules", () => {
-  it("should execute rule no-unsanitized/DOM/method", () => {
-    const configDOM = config[config.length - 2][0];
-    const rule = configDOM.plugins["no-unsanitized"].rules.method;
-    const valid = [
-      { code: "n.insertAdjacentHTML('afterend', 'meh');" },
-      { code: "n.insertAdjacentHTML('afterend', '<br>');" },
-    ];
-    const invalid = [
-      {
-        code: "node.insertAdjacentHTML('beforebegin', htmlString);",
-        errors: [
-          { message: "Unsafe call to node.insertAdjacentHTML for argument 1" },
-        ],
-      },
-      {
-        code: "node.insertAdjacentHTML('beforebegin', template.getHTML());",
-        errors: [
-          { message: "Unsafe call to node.insertAdjacentHTML for argument 1" },
-        ],
-      },
-    ];
-    tester.run("no-unsanitized/DOM/property", rule, { valid, invalid });
-    expect(Array.isArray(config)).toBeTruthy();
-  });
+let configDOM = config[0];
+let rule = configDOM.plugins["no-unsanitized"].rules.method;
+let valid = [
+  { code: "n.insertAdjacentHTML('afterend', 'meh');" },
+  { code: "n.insertAdjacentHTML('afterend', '<br>');" },
+];
+let invalid = [
+  {
+    code: "node.insertAdjacentHTML('beforebegin', htmlString);",
+    errors: [
+      { message: "Unsafe call to node.insertAdjacentHTML for argument 1" },
+    ],
+  },
+  {
+    code: "node.insertAdjacentHTML('beforebegin', template.getHTML());",
+    errors: [
+      { message: "Unsafe call to node.insertAdjacentHTML for argument 1" },
+    ],
+  },
+];
+tester.run("no-unsanitized/DOM/method", rule, { valid, invalid });
+expect(configDOM).toBeTypeOf("object");
+expect(rule).toBeTypeOf("object");
 
-  it("should execute rule no-unsanitized/DOM/property", () => {
-    const configDOM = config[config.length - 2][0];
-    const rule = configDOM.plugins["no-unsanitized"].rules.property;
-    const valid = [
-      { code: "a.innerHTML = 'test';" },
-      { code: "u.innerHTML = '<span>${lulz}</span>';" },
-    ];
-    const invalid = [
-      {
-        code: "m.innerHTML = htmlString;",
-        errors: [{ message: "Unsafe assignment to innerHTML" }],
-      },
-      {
-        code: "a.innerHTML += template.toHtml();",
-        errors: [{ message: "Unsafe assignment to innerHTML" }],
-      },
-    ];
-    tester.run("no-unsanitized/DOM/property", rule, { valid, invalid });
-    expect(Array.isArray(config)).toBeTruthy();
+rule = configDOM.plugins["no-unsanitized"].rules.property;
+valid = [
+  { code: "a.innerHTML = 'test';" },
+  { code: "u.innerHTML = '<span>${lulz}</span>';" },
+];
+invalid = [
+  {
+    code: "m.innerHTML = htmlString;",
+    errors: [{ message: "Unsafe assignment to innerHTML" }],
+  },
+  {
+    code: "a.innerHTML += template.toHtml();",
+    errors: [{ message: "Unsafe assignment to innerHTML" }],
+  },
+];
+tester.run("no-unsanitized/DOM/property", rule, { valid, invalid });
+expect(configDOM).toBeTypeOf("object");
+expect(rule).toBeTypeOf("object");
+
+describe("ESLint config", () => {
+  it("it should be mlauffer-ui5", () => {
+    const configUi5 = config[config.length - 1];
+    expect(configUi5.name).toEqual("mlauffer-ui5");
   });
 });
