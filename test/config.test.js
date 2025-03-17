da2c21de17fd22e3dcfd40b5111635a659895655
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2025-03-17 15:42:41 +1000

    feat: Use defineConfig + Extends

--- test/config.test.js
+++ test/config.test.js
@@ -3,16 +3,14 @@ import config from "../lib/index.js";
 
 describe("Config object", () => {
   it("should export rules", () => {
-    const configDOM = config[0];
     expect(Array.isArray(config)).toBeTruthy();
     expect(config.length).toBeGreaterThan(0);
-    expect(configDOM.name).toEqual(undefined);
-    expect(configDOM.plugins).toBeTypeOf("object");
-    expect(configDOM.rules).toBeTypeOf("object");
     expect(config).toEqual(
       expect.arrayContaining([
+        expect.objectContaining({
+          name: expect.stringMatching(/mlauffer-nodejs$/),
+        }),
         expect.objectContaining({ name: "mlauffer-ui5" }),
-        expect.objectContaining({ name: "mlauffer-nodejs" }),
       ]),
     );
   });
